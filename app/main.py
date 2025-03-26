576148dae0927f4c31f84cb0d6110250669bf20b
junhao <junhao.zhu@alumnos.upm.es>
2025-03-26 17:04:37 +0100

    ruff cambiado

--- app/main.py
+++ app/main.py
@@ -4,25 +4,12 @@ from fastapi.responses import RedirectResponse
 from app.application.get_dice_roll import get_dice_roll
 
 
-
-
-
-
-
-
-
-
-
-
 def create_app() -> FastAPI:
     app = FastAPI()
 
-    app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False,)
+    app.add_api_route(path="/", endpoint=lambda: RedirectResponse(app.url_path_for(get_dice_roll.__name__)), methods=["GET"], include_in_schema=False)
 
-    app.add_api_route(
-        path="/dice/roll",
-        endpoint=get_dice_roll
-    )
+    app.add_api_route(path="/dice/roll", endpoint=get_dice_roll)
 
     return app
 
